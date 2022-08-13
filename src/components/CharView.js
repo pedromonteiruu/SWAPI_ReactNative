5d952a38391f58aa279b4a239c0485dfe5c5500b
pedromonteiruu <pedro.monteiro.silva@ibm.com>
2022-08-12 22:25:08 -0300

    null response in search

--- src/components/CharView.js
+++ src/components/CharView.js
@@ -2,27 +2,38 @@ import React,{useEffect, useState} from 'react'
 import {Text, SafeAreaView, StyleSheet, View, TouchableOpacity, ActivityIndicator } from 'react-native'
 
 export const CharView = ({dados}) => {
-    // const {
-    //   gender,
-    //   name,
-    //   height,
-    //   birth_year,
-    //   hair_color,
-    //   skin_color,
-    //   eye_color,
-    //   mass } = dados
+    if (!dados) {
+      return <View style={[style.page]}><Text style={[style.logs,
+        { fontSize:32
+        }]}>Char don't exist</Text></View>
+    }
+
+    const {
+      gender,
+      name,
+      height,
+      birth_year,
+      hair_color,
+      skin_color,
+      eye_color,
+      mass } = dados
     
     return (
         <View style={[style.page]}>
-        <Text style={[style.logs, {fontSize:20, borderBottomColor: 'white', borderBottomWidth:3}]}>        
-            {dados?.name}</Text>
-          <Text style={style.logs}>Gender:       {dados?.gender}</Text>
-          <Text style={style.logs}>Height:        {dados?.height}</Text>
-          <Text style={style.logs}>Birth Year:  {dados?.birth_year}</Text>
-          <Text style={style.logs}>Hair Color:  {dados?.hair_color}</Text>
-          <Text style={style.logs}>Skin Color:  {dados?.skin_color}</Text>
-          <Text style={style.logs}>Eye Color:   {dados?.eye_color}</Text>
-          <Text style={style.logs}>Mass:          {dados?.mass}</Text>
+        <Text style={[style.logs,
+          { fontSize:20,
+            borderBottomColor: 'white',
+            borderBottomWidth:3
+          }]}>        
+            {name}
+          </Text>
+          <Text style={style.logs}>Gender:       {gender}</Text>
+          <Text style={style.logs}>Height:        {height}</Text>
+          <Text style={style.logs}>Birth Year:  {birth_year}</Text>
+          <Text style={style.logs}>Hair Color:  {hair_color}</Text>
+          <Text style={style.logs}>Skin Color:  {skin_color}</Text>
+          <Text style={style.logs}>Eye Color:   {eye_color}</Text>
+          <Text style={style.logs}>Mass:          {mass}</Text>
           
         </View>
     )
